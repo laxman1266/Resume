83bd290409a3e1dfa6647ba757980e5d3bfb6df2
ramyasashi <ramyakrishna676@gmail.com>
2020-03-16 08:54:35 +0530

    added ejs

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
+app.set("view engine",'ejs');
+app.set('views',path.join(__dirname,'/templates'));
 app.get('',(req,res) => {
-    res.send("Welcome to Node js")
+    res.render("index")
 })
 app.listen(port,() => {
     console.log("Server runs on :"+port);
