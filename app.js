065ab5cbfdccf8a4286c3720eb4b862ac4939fd2
ramyasashi <ramyakrishna676@gmail.com>
2020-03-16 08:56:33 +0530

    added ejs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 app.set("view engine",'ejs');
-app.set('views',path.join(__dirname,'/templates'));
+app.set('views',path.join(__dirname,'./templates'));
 app.get('',(req,res) => {
     res.render("index")
 })
